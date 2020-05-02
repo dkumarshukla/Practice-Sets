eeb2c195fc554caea7105f43fe2a21ca40d71b2c
dkumarshukla <38921178+dkumarshukla@users.noreply.github.com>
2020-05-02 10:59:38 +0100

    Add files via upload

---
+++ MinimumCost.java
@@ -0,0 +1,46 @@
+package com.dks;
+
+import java.util.*;
+
+public class MinimumCost {
+
+	static long minimumCost(int p[], int n) {
+
+		Arrays.sort(p);
+		int tCost = 0;
+
+		if (n <= 3) {
+			for (int i = 0; i < n; i++) {
+				tCost += p[i];
+			}
+		} else {
+			for (int i = n - 1; i > 1; i -= 2) {
+				if (i == 2) {
+					tCost += p[2] + p[0];
+				} else {
+					int pfirst = p[i] + p[0] + 2 * p[1];
+					int psecond = p[i] + p[i - 1] + 2 * p[0];
+					tCost += Math.min(pfirst, psecond);
+				}
+			}
+			if (n == 1) {
+				tCost += p[0];
+			} else {
+				tCost += p[1];
+			}
+		}
+		return tCost;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please Enter the no. of people in Village: ");
+		int number = s.nextInt();
+		int[] p = new int[number];
+		for (int i = 0; i < number; i++) {
+			System.out.println("Enter the p of Person " + i);
+			p[i] = s.nextInt();
+		}
+		System.out.println(minimumCost(p, number));
+	}
+}
